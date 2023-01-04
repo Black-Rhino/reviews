5da9ac22c2441b3b511d27e74c56c857c2f2ae5d
Linh <linhscholar71@gmail.com>
2023-01-03 21:35:49 -0500

    k6 load testing

--- k6/script.js
+++ k6/script.js
@@ -1,3 +1,16 @@
 import http from 'k6/http';
-
 import { sleep } from 'k6';
+
+export const options = {
+  vus: 1000,
+  duration: '30s',
+  thresholds: {
+    http_req_failed: ['rate<0.01'],
+    http_req_duration: ['p(95)<1500']
+  }
+}
+
+export default function () {
+  http.get('http://localhost:3000/reviews/1/list');
+  sleep(1);
+}
